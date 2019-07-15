9aefde2cbca68f94b5807d204ae73bd8727f2223
Alim Rahardian <49728099+alimrahardian@users.noreply.github.com>
2019-07-15 22:58:16 +0700

    Add files via upload

--- pokemon_app.py
+++ pokemon_app.py
@@ -32,48 +32,48 @@ randomFr.fit(xtrain, ytrain)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        # try:
-        # hp, attack, def, special attack, special def, speed
-        url = 'https://pokeapi.co/api/v2/pokemon/{}/'
-        cari = request.form['cari1']
-        req_pokemon = requests.get(url.format(cari)).json()
-        poke_stats = {
-            'nama' : req_pokemon['name'],
-            'gambar' : req_pokemon['sprites']['front_default'],
-            'id' : req_pokemon['id'],
-            'HP' : pokemon['HP'].iloc[req_pokemon['id']],
-            'Attack' : pokemon['Attack'].iloc[req_pokemon['id']],
-            'Defense' : pokemon['Defense'].iloc[req_pokemon['id']],
-            'Sp_Atk' : pokemon['Sp. Atk'].iloc[req_pokemon['id']],
-            'Sp_Def' : pokemon['Sp. Def'].iloc[req_pokemon['id']],
-            'Speed' : pokemon['Speed'].iloc[req_pokemon['id']]
-        }
+        try:
+            # hp, attack, def, special attack, special def, speed
+            url = 'https://pokeapi.co/api/v2/pokemon/{}/'
+            cari = request.form['cari1']
+            req_pokemon = requests.get(url.format(cari.lower())).json()
+            poke_stats = {
+                'nama' : req_pokemon['name'],
+                'gambar' : req_pokemon['sprites']['front_default'],
+                'id' : req_pokemon['id'],
+                'HP' : pokemon['HP'].iloc[req_pokemon['id']],
+                'Attack' : pokemon['Attack'].iloc[req_pokemon['id']],
+                'Defense' : pokemon['Defense'].iloc[req_pokemon['id']],
+                'Sp_Atk' : pokemon['Sp. Atk'].iloc[req_pokemon['id']],
+                'Sp_Def' : pokemon['Sp. Def'].iloc[req_pokemon['id']],
+                'Speed' : pokemon['Speed'].iloc[req_pokemon['id']]
+            }
 
-        cari2 = request.form['cari2']
-        req_pokemon2 = requests.get(url.format(cari2)).json()
-        poke_stats2 = {
-            'nama' : req_pokemon2['name'],
-            'gambar' : req_pokemon2['sprites']['front_default'],
-            'id' : req_pokemon2['id'],
-            'HP' : pokemon['HP'].iloc[req_pokemon2['id']],
-            'Attack' : pokemon['Attack'].iloc[req_pokemon2['id']],
-            'Defense' : pokemon['Defense'].iloc[req_pokemon2['id']],
-            'Sp_Atk' : pokemon['Sp. Atk'].iloc[req_pokemon2['id']],
-            'Sp_Def' : pokemon['Sp. Def'].iloc[req_pokemon2['id']],
-            'Speed' : pokemon['Speed'].iloc[req_pokemon2['id']]
-        }
+            cari2 = request.form['cari2']
+            req_pokemon2 = requests.get(url.format(cari2.lower())).json()
+            poke_stats2 = {
+                'nama' : req_pokemon2['name'],
+                'gambar' : req_pokemon2['sprites']['front_default'],
+                'id' : req_pokemon2['id'],
+                'HP' : pokemon['HP'].iloc[req_pokemon2['id']],
+                'Attack' : pokemon['Attack'].iloc[req_pokemon2['id']],
+                'Defense' : pokemon['Defense'].iloc[req_pokemon2['id']],
+                'Sp_Atk' : pokemon['Sp. Atk'].iloc[req_pokemon2['id']],
+                'Sp_Def' : pokemon['Sp. Def'].iloc[req_pokemon2['id']],
+                'Speed' : pokemon['Speed'].iloc[req_pokemon2['id']]
+            }
 
-        
-        combat_proba = {'a' : randomFr.predict_proba([[poke_stats['id'], poke_stats2['id']]])[0][1]}
+            
+            combat_proba = {'a' : randomFr.predict_proba([[poke_stats['id'], poke_stats2['id']]])[0][1]}
 
-        return render_template (
-            'result.html', 
-            poke_stats=poke_stats, 
-            poke_stats2=poke_stats2,
-            combat_proba=combat_proba
-            )
-        # except:
-        #     return redirect ('error.html')
+            return render_template (
+                'result.html', 
+                poke_stats=poke_stats, 
+                poke_stats2=poke_stats2,
+                combat_proba=combat_proba
+                )
+        except:
+            return redirect ('error.html')
     else:
         return render_template ('home.html')
 
